db8aa7613c40a6f57a7e813b46864f1bb80cd19b
Goran Atanasov <Goran Atanasov>
2016-04-10 05:39:47 +0200

    Initial commit

---
+++ mobile/src/main/java/ltsu/atanasovgoran/lightthestreetup/MainActivity.java
@@ -0,0 +1,13 @@
+package ltsu.atanasovgoran.lightthestreetup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
